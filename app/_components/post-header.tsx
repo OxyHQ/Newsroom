86b9809134410d017f08bd352eef4705f9836c07
AlbertIsernAlvarez <albert.isern.alvarez@gmail.com>
2024-04-07 12:15:02 +0200

    Refactor post-header.tsx to remove unused code and update layout

--- app/_components/post-header.tsx
+++ app/_components/post-header.tsx
@@ -14,14 +14,12 @@ type Props = {
 export function PostHeader({ title, coverImage, date, author }: Props) {
   return (
     <>
+      <div className="mb-6 text-lg">
+        <DateFormatter dateString={date} />
+      </div>
       <PostTitle>{title}</PostTitle>
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} />
-      </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="mb-6 text-lg">
-          <DateFormatter dateString={date} />
-        </div>
       </div>
     </>
   );
