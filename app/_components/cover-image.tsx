65058d31b17e14e0f2a01fe52313120fd2c5c9b1
AlbertIsernAlvarez <albert.isern.alvarez@gmail.com>
2024-04-07 14:09:45 +0200

    Update cover-image component with rounded corners

--- app/_components/cover-image.tsx
+++ app/_components/cover-image.tsx
@@ -14,7 +14,7 @@ const coverImage = ({ title, src, slug }: Props) => {
       src={src}
       alt={`Cover Image for ${title}`}
       className={cn("shadow-sm w-full", {
-        "hover:shadow-lg transition-shadow duration-200": slug,
+        "hover:shadow-lg transition-shadow duration-200 rounded-[3em]": slug,
       })}
       width={1300}
       height={630}
