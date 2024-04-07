0070cc4b4c46b0575c88a23de073356a30128695
AlbertIsernAlvarez <albert.isern.alvarez@gmail.com>
2024-04-07 11:32:21 +0200

    Update next.config.mjs to add remotePatterns for images

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["prod-files-secure.s3.us-west-2.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
