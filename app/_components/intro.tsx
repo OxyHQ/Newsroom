b89b75e83ce354f2c999e6416f2e4e019e36f0a4
AlbertIsernAlvarez <albert.isern.alvarez@gmail.com>
2024-04-07 11:16:28 +0200

    Update intro.tsx to remove unused import and fix text

--- app/_components/intro.tsx
+++ app/_components/intro.tsx
@@ -1,5 +1,3 @@
-import { CMS_NAME } from "@/lib/constants";
-
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
@@ -14,7 +12,7 @@ export function Intro() {
         >
           Next.js
         </a>{" "}
-        and {CMS_NAME}.
+        and.
       </h4>
     </section>
   );
