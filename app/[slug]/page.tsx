db34da0c0be72c09e2c8c038313f03af25f8524b
AlbertIsernAlvarez <albert.isern.alvarez@gmail.com>
2024-04-07 11:03:36 +0200

    Update dependencies in package.json and refactor page.tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,6 +1,12 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import Image from "next/image";
+import markdownToHtml from "@/lib/markdownToHtml";
+import Alert from "@/app/_components/alert";
+import Container from "@/app/_components/container";
+import Header from "@/app/_components/header";
+import { PostBody } from "@/app/_components/post-body";
+import { PostHeader } from "@/app/_components/post-header";
 
 async function getData(slug: string) {
   const res = await fetch(
@@ -16,39 +22,58 @@ async function getData(slug: string) {
   return data;
 }
 
-export const metadata: Metadata = {
-  title: "",
-  description: "",
-  openGraph: {},
-};
+export default async function Post({ params }: { params: { slug: string } }) {
+  const post = await getData(params.slug);
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const data = await getData(params.slug);
-
-  metadata.title = data?.title + " - Peable Newsroom" || "Peable Newsroom";
-  metadata.description = data?.content
-    ? data.content.slice(0, 100) + "..."
-    : "";
-
-  // Ensure that metadata.openGraph is an object before assigning the images property
-  if (!metadata.openGraph) {
-    metadata.openGraph = {};
+  if (!post) {
+    return notFound();
   }
 
-  metadata.openGraph.images = [
-    {
-      url: data.image,
-      width: 800,
-      height: 400,
-      alt: data.title,
-    },
-  ];
+  const content = await markdownToHtml(post.content || "");
 
   return (
-    <article>
-      <Image src={data.image} alt={data.title} width={800} height={400} />
-      <h1 className="text-2xl font-bold">{data.title}</h1>
-      <p className="text-gray-500">{data.content}</p>
-    </article>
+    <main>
+      <Container>
+        <Header />
+        <article className="mb-32">
+          <PostHeader
+            title={post?.title}
+            coverImage={post?.image}
+            date={post?.date.default}
+            author={post?.author}
+          />
+          <PostBody content={content} />
+        </article>
+      </Container>
+    </main>
   );
+}
+
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+export function generateMetadata({ params }: Params): Metadata {
+  const post = {
+    title: "Next.js Blog Example with Markdown",
+    ogImage: {
+      url: "",
+    },
+  };
+
+  if (!post) {
+    return notFound();
+  }
+
+  const title = `${post.title} | Next.js Blog Example with`;
+
+  return {
+    title,
+    openGraph: {
+      title,
+      images: [post.ogImage.url],
+    },
+  };
 }
